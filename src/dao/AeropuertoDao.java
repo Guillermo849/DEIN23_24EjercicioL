ddff4d38748de536b291a708881126d58fedbbb5
Guillermo849 <orivealonsoguillermo@gmail.com>
2023-10-22 21:00:13 +0200

    ADD Metodo Añadir Aeropuertos a la BDD

--- src/dao/AeropuertoDao.java
+++ src/dao/AeropuertoDao.java
@@ -14,42 +14,47 @@ public class AeropuertoDao {
 
 	private ConexionBDD conexion;
 
-	/* Devuelve una lista de los aeropuertos privados almacenadas en la base de datos */
+	/*
+	 * Devuelve una lista de los aeropuertos privados almacenadas en la base de
+	 * datos
+	 */
 	public ObservableList<Aeropuertos> cargarAeropuertosPrivados() {
 
 		ObservableList<Aeropuertos> aeropuertosPrivados = FXCollections.observableArrayList();
 		try {
 			conexion = new ConexionBDD();
-			
-			/* Recogemos la información de los IDs y número de socios de los aeropuertos privados */
+
+			/*
+			 * Recogemos la información de los IDs y número de socios de los aeropuertos
+			 * privados
+			 */
 			String consulta = "select * from aeropuertos_privados";
 			PreparedStatement pstmt = conexion.getConexion().prepareStatement(consulta);
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			HashMap<Integer, Integer> mapPrivados = new HashMap<Integer, Integer>();
-			
+
 			while (rs.next()) {
 				int idAeropuertos = rs.getInt("id_aeropuerto");
 				int numSocios = rs.getInt("numero_socios");
 				mapPrivados.put(idAeropuertos, numSocios);
 			}
 			rs.close();
-			
-			
+
 			/* Recogemos los aeropuertos que su id este dentro del HashMap mapPrivados */
-			
+
 			consulta = "SELECT * FROM aeropuertos";
 			pstmt = conexion.getConexion().prepareStatement(consulta);
 			rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
 				if (mapPrivados.containsKey(rs.getInt("id"))) {
-					
+
 					String consultaDireccion = "SELECT * FROM direcciones WHERE id LIKE " + rs.getInt("id_direccion");
-					
+
 					PreparedStatement pstmtDireccion = conexion.getConexion().prepareStatement(consultaDireccion);
 					ResultSet rsDireccion = pstmtDireccion.executeQuery();
-					
+
 					int id = rs.getInt("id");
 					String nombre = rs.getString("nombre");
 					String pais = rsDireccion.getString("pais");
@@ -59,17 +64,18 @@ public class AeropuertoDao {
 					int anio = rs.getInt("anio_inaguracion");
 					int capacidad = rs.getInt("capacidad");
 					int numSocios = mapPrivados.get(rs.getInt("id"));
-					
-					Aeropuertos a = new Aeropuertos(id, nombre, pais, ciudad, calle, numero, anio, capacidad, numSocios);
-					
+
+					Aeropuertos a = new Aeropuertos(id, nombre, pais, ciudad, calle, numero, anio, capacidad,
+							numSocios);
+
 					aeropuertosPrivados.add(a);
 					rsDireccion.close();
-					
+
 				}
 			}
-			
+
 			rs.close();
-			
+
 			conexion.CloseConexion();
 
 		} catch (SQLException e) {
@@ -77,21 +83,27 @@ public class AeropuertoDao {
 		}
 		return aeropuertosPrivados;
 	}
-	
-	/* Devuelve una lista de los aeropuertos publicos almacenadas en la base de datos */
+
+	/*
+	 * Devuelve una lista de los aeropuertos publicos almacenadas en la base de
+	 * datos
+	 */
 	public ObservableList<Aeropuertos> cargarAeropuertosPublicos() {
 
 		ObservableList<Aeropuertos> aeropuertosPublicos = FXCollections.observableArrayList();
 		try {
 			conexion = new ConexionBDD();
-			
-			/* Recogemos la información de los IDs y número de socios de los aeropuertos publicos */
+
+			/*
+			 * Recogemos la información de los IDs y número de socios de los aeropuertos
+			 * publicos
+			 */
 			String consulta = "select * from aeropuertos_publicos";
 			PreparedStatement pstmt = conexion.getConexion().prepareStatement(consulta);
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			HashMap<Integer, Integer[]> mapPublicos = new HashMap<Integer, Integer[]>();
-			
+
 			while (rs.next()) {
 				int idAeropuertos = rs.getInt("id_aeropuerto");
 				int financiacion = rs.getInt("financiacion");
@@ -102,22 +114,21 @@ public class AeropuertoDao {
 				mapPublicos.put(idAeropuertos, infoPublicos);
 			}
 			rs.close();
-			
-			
+
 			/* Recogemos los aeropuertos que su id este dentro del HashMap mapPublicos */
-			
+
 			consulta = "SELECT * FROM aeropuertos";
 			pstmt = conexion.getConexion().prepareStatement(consulta);
 			rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
 				if (mapPublicos.containsKey(rs.getInt("id"))) {
-					
+
 					String consultaDireccion = "SELECT * FROM direcciones WHERE id LIKE " + rs.getInt("id_direccion");
-					
+
 					PreparedStatement pstmtDireccion = conexion.getConexion().prepareStatement(consultaDireccion);
 					ResultSet rsDireccion = pstmtDireccion.executeQuery();
-					
+
 					int id = rs.getInt("id");
 					String nombre = rs.getString("nombre");
 					String pais = rsDireccion.getString("pais");
@@ -128,17 +139,18 @@ public class AeropuertoDao {
 					int capacidad = rs.getInt("capacidad");
 					int financiacion = mapPublicos.get(rs.getInt("id"))[0];
 					int numTrabajadores = mapPublicos.get(rs.getInt("id"))[1];
-					
-					Aeropuertos a = new Aeropuertos(id, nombre, pais, ciudad, calle, numero, anio, capacidad, financiacion, numTrabajadores);
-					
+
+					Aeropuertos a = new Aeropuertos(id, nombre, pais, ciudad, calle, numero, anio, capacidad,
+							financiacion, numTrabajadores);
+
 					aeropuertosPublicos.add(a);
 					rsDireccion.close();
-					
+
 				}
 			}
-			
+
 			rs.close();
-			
+
 			conexion.CloseConexion();
 
 		} catch (SQLException e) {
@@ -147,4 +159,79 @@ public class AeropuertoDao {
 		return aeropuertosPublicos;
 	}
 
+	public void aniadirAeropuerto(Aeropuertos aeropuerto, boolean privado) {
+
+		try {
+			conexion = new ConexionBDD();
+
+			/* Recoge la ip de la direccion */
+			String consulta = "SELECT id FROM direcciones WHERE pais LIKE '" + aeropuerto.getPais()
+					+ "' AND ciudad LIKE '" + aeropuerto.getCalle() + "' AND calle LIKE '" + aeropuerto.getCalle()
+					+ "' AND numero == " + aeropuerto.getNumero();
+			PreparedStatement pstmt = conexion.getConexion().prepareStatement(consulta);
+			ResultSet rs = pstmt.executeQuery();
+
+			int idDireccion;
+
+			/* Si no existe la direccion, creará una nueva */
+			if (rs.next() == false) {
+				aniadirDireccion(aeropuerto.getPais(), aeropuerto.getCalle(), aeropuerto.getCalle(),
+						aeropuerto.getNumero());
+				rs.close();
+				/* Recoge la ip de la nueva direccion */
+				pstmt = conexion.getConexion().prepareStatement(consulta);
+				rs = pstmt.executeQuery();
+				idDireccion = rs.getInt("id");
+				rs.close();
+
+				/* Si ya existe la direccion guardará el id de está */
+			} else {
+				idDireccion = rs.getInt("id");
+				rs.close();
+			}
+
+			consulta = "INSERT INTO aeropuertos(nombre,anio_inauguracion, capacidad, id_direccion) VALUES('"
+					+ aeropuerto.getNombre() + "'," + aeropuerto.getAnio() + "," + aeropuerto.getCapacidad() + ","
+					+ idDireccion + ")";
+
+			/*
+			 * Añade el nuevo aeropuerto al aeropuerto público o privado dependiendo de cual
+			 * se haya seleccionado
+			 */
+			if (privado == true) {
+
+				consulta = "INSERT INTO aeropuertos_privados VALUES (" + aeropuerto.getId() + ","
+						+ aeropuerto.getNumSocios() + ")";
+
+			} else {
+				consulta = "INSERT INTO aeropuertos_publicos VALUES (" + aeropuerto.getId() + ","
+						+ aeropuerto.getFinanciacion() + "," + aeropuerto.getNumTrabajadores() + ")";
+			}
+
+			pstmt = conexion.getConexion().prepareStatement(consulta);
+			pstmt.executeUpdate();
+
+			conexion.CloseConexion();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/* Añade la direccion del aeropuerto */
+	private void aniadirDireccion(String pais, String ciudad, String calle, int numero) {
+
+		try {
+			conexion = new ConexionBDD();
+			String consulta = "INSERT INTO persona(pais,ciudad,calle,numero) VALUES('" + pais + "','" + ciudad + "','"
+					+ calle + "'" + numero + ");";
+			PreparedStatement pstmt = conexion.getConexion().prepareStatement(consulta);
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
